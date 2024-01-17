46290edb5c42cdab1b797b72bd026dfe3593eb21
Kirsten Frager <kirsten@hellostake.com>
2024-01-17 13:32:06 +0800

    ref: ios pods and files renamed to new project name HellostakeInstagram

---
+++ ios/HellostakeInstagram/Source/HellostakeInstagram.h
@@ -0,0 +1,19 @@
+//
+//  HellostakeInstagram.h
+//  HellostakeInstagram
+//
+//  Created by Michael Oberwasserlechner on 19.12.18.
+//  Copyright © 2018 BYTEOWLS Software & Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HellostakeInstagram.
+FOUNDATION_EXPORT double HellostakeInstagramVersionNumber;
+
+//! Project version string for HellostakeInstagram.
+FOUNDATION_EXPORT const unsigned char HellostakeInstagramVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HellostakeInstagram/PublicHeader.h>
+
+
